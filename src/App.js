d0ab9a6b3f71c0841820273dfba152b319ef35cf
Boqdan2005 <cvyrydboqdan@gmail.com>
2020-09-25 16:01:05 +0300

    added lazy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
-import HomePage from './views/HomePage/HomePage';
-import MoviesPage from './views/MoviesPage/MoviesPage';
-import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
+// import HomePage from './views/HomePage/HomePage';
+// import MoviesPage from './views/MoviesPage/MoviesPage';
+// import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
 import NotFound from './views/NotFound/NotFound';
+import Loader from './Components/Loader/Loader';
 import './App.module.css';
 import routes from './views/routes';
+
+const HomePage = lazy(() => import('./views/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./views/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage/MovieDetailsPage'));
 
 function App() {
   return (
     <Layout>
-     <Switch>
-       <Route path={routes.home} exact component={HomePage} />
-       <Route path={routes.movies} exact component={MoviesPage} />
-       <Route path={routes.movieId} component={MovieDetailsPage} />
-       <Route component={NotFound} />
-     </Switch>
+      <Suspense fallback={<Loader />} >
+        <Switch>
+         <Route path={routes.home} exact component={HomePage} />
+         <Route path={routes.movies} exact component={MoviesPage} />
+         <Route path={routes.movieId} component={MovieDetailsPage} />
+         <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+     
     </Layout>
   );
 }
